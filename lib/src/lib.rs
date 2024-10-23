83249f0f337afac571f7d32fdbbdc1a78c227890
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-10-23 22:24:43 +0100

    conststruct_uint

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,3 +1,9 @@
+use uint::construct_uint;
+construct_uint! {
+    // Construct an unsigned 256-bit integer
+    // consisting of 4 x 64-bit words
+    pub struct U256(4);
+}
 pub mod sha256;
 pub mod types;
 pub mod util;
