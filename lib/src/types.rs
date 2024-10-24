48a0fb05cc7ff8bcdd2e216892902b5d339699e2
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-10-24 22:41:35 +0100

    library types blockHeader

--- lib/src/types.rs
+++ lib/src/types.rs
@@ -1,3 +1,5 @@
+use crate::U256;
+
 pub struct Blockchain {
     pub blocks: Vec<Block>,
 }
@@ -27,5 +29,37 @@ impl Block {
         unimplemented!()
     }
 }
-pub struct BlockHeader;
+pub struct BlockHeader {
+    /// Timestamp of the block
+    pub timestamp: u64,
+    /// Nonce used to mine the block
+    pub nonce: u64,
+    /// Hash of the previous block
+    pub prev_block_hash: [u8; 32],
+    /// Merkle root of the block's transactions
+    pub merkle_root: [u8; 32],
+    /// target
+    pub target: U256,
+}
+
+impl BlockHeader {
+    pub fn new(
+        timestamp: u64,
+        nonce: u64,
+        prev_block_hash: [u8; 32],
+        merkle_root: [u8; 32],
+        target: U256,
+    ) -> Self {
+        BlockHeader {
+            timestamp,
+            nonce,
+            prev_block_hash,
+            merkle_root,
+            target,
+        }
+    }
+    pub fn hash(&self) -> ! {
+        unimplemented!()
+    }
+}
 pub struct Transaction;
