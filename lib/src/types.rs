5997cc7339723eebc6045064a8d0430574c8d861
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-10-24 22:26:47 +0100

    library types block

--- lib/src/types.rs
+++ lib/src/types.rs
@@ -3,13 +3,29 @@ pub struct Blockchain {
 }
 
 impl Blockchain {
-    pub fn new() ->  Self {
+    pub fn new() -> Self {
         Blockchain { blocks: vec![] }
     }
     pub fn add_block(&mut self, block: Block) {
         self.blocks.push(block);
     }
 }
-pub struct Block;
+
+pub struct Block {
+    pub header: BlockHeader,
+    pub transactions: Vec<Transaction>,
+}
+
+impl Block {
+    pub fn new(header: BlockHeader, transactions: Vec<Transaction>) -> Self {
+        Block {
+            header,
+            transactions,
+        }
+    }
+    pub fn hash(&self) -> ! {
+        unimplemented!()
+    }
+}
 pub struct BlockHeader;
 pub struct Transaction;
