9746d2793d25fdd1e32ef504f00e70983b1f9728
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-10-25 23:31:53 +0100

    Types for Merkle root, public and private key, Hash and Signature

---
+++ lib/src/sha256.rs
@@ -0,0 +1,31 @@
+use crate::U256;
+use serde::Serialize;
+use sha256::digest;
+
+#[derive(Clone, Copy, Serialize)]
+pub struct Hash(U256);
+impl Hash {
+    // hash anything that can be serialized via ciborium
+    pub fn hash<T: serde::Serialize>(data: &T) -> Self {
+        let mut serialized: Vec<u8> = vec![];
+        if let Err(e) = ciborium::into_writer(data, &mut serialized) {
+            panic!(
+                "Failed to serialize data: {:?}. \
+                This should not happen",
+                e
+            );
+        }
+        let hash = digest(&serialized);
+        let hash_bytes = hex::decode(hash).unwrap();
+        let hash_array: [u8; 32] = hash_bytes.as_slice().try_into().unwrap();
+        Hash(U256::from(hash_array))
+    }
+    // check if a hash matches a target
+    pub fn matches_target(&self, target: U256) -> bool {
+        self.0 <= target
+    }
+    // zero hash
+    pub fn zero() -> Self {
+        Hash(U256::zero())
+    }
+}
