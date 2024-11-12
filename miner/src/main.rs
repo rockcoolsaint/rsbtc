66facdb1a083be749fd7157d11c4aa5ae2b19830
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-11-12 23:11:34 +0100

    key_gen

--- miner/src/main.rs
+++ miner/src/main.rs
@@ -1,8 +1,10 @@
 use std::{env, process::exit};
+use tokio::net::TcpStream;
 
-use btclib::{types::Block, util::Saveable};
+use btclib::{crypto::PublicKey, types::Block, util::Saveable};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // parse block path and steps count from the
     // first and second argument respectively
     let (path, steps) = if let (Some(arg), Some(arg2)) =
@@ -34,4 +36,33 @@ fn main() {
     // print mined block and its hash
     println!("final: {:#?}", block);
     println!("hash: {}", block.header.hash());
+
+    fn usage() -> ! {
+        eprintln!(
+            "Usage: {} <address> <public_key_file",
+            env::args().next().unwrap()
+        );
+        exit(1);
+    }
+
+    let address = match  env::args().nth(1) {
+        Some(address) => address,
+        None => usage(),
+    };
+    let public_key_file = match env::args().nth(2) {
+        Some(public_key_file) => public_key_file,
+        None => usage(),
+    };
+    let Ok(public_key) =
+        PublicKey::load_from_file(&public_key_file)
+    else {
+        eprintln!(
+            "Error reading public key from file {}",
+            public_key_file
+        );
+        exit(1);
+    };
+    println!(
+        "Connecting to {address} to mine with {public_key:?}",
+    );
 }
