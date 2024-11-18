5afefdc265f5c0b327d41f91f3d49ec053b1cac8
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-11-18 23:00:58 +0100

    CLI struct and command config

--- wallet/src/main.rs
+++ wallet/src/main.rs
@@ -1,3 +1,30 @@
+use anyhow::Result;
+use clap::{Command, Parser, Subcommand};
+use kanal::bounded;
+use tokio::time::{self, Duration};
+use std::io::{self, Write};
+use std::path::PathBuf;
+use btclib::types::Transaction;
+
+mod core;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+    #[arg(short, long, value_name = "ADDRESS")]
+    node: Option<String>,
+}
+#[derive(Subcommand)]
+enum Commands {
+    GenerateConfig {
+        #[arg(short, long, value_name = "FILE")]
+        output: PathBuf,
+    },
+}
 fn main() {
     println!("Hello, world!");
 }
